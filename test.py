7e6dff7c1acd4dc06031715dbde01619559016b0
Cuner <469464794@qq.com>
2018-10-12 14:05:40 +0800

    add

--- test.py
+++ test.py
@@ -7,7 +7,10 @@ newInstance.auto_login(hotReload=True)
 friends = newInstance.get_friends()
 
 for friend in friends:
-    newInstance.send('', toUserName=friend['UserName'])
+    if friend['NickName'] == '火云邪神本尊' :
+        r = newInstance.send(" ", toUserName=friend['UserName'])
+    elif friend['RemarkName'] == '拉黑了我' :
+        r = newInstance.send(" ", toUserName=friend['UserName'])
 
 @newInstance.msg_register(itchat.content.TEXT)
 def text_reply(msg):
