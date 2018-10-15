4b554b2183394af10ce10826f3f36595834afdcf
Cuner <469464794@qq.com>
2018-10-15 15:37:11 +0800

    add

--- test.py
+++ test.py
@@ -9,9 +9,9 @@ friends = newInstance.get_friends()
 
 for friend in friends:
     if friend['NickName'] == '火云邪神本尊':
-        r = newInstance.send("", toUserName=friend['UserName'])
+        r = newInstance.send(" ", toUserName=friend['UserName'])
     elif friend['RemarkName'] == '拉黑了我':
-        r = newInstance.send("", toUserName=friend['UserName'])
+        r = newInstance.send(" ", toUserName=friend['UserName'])
 
 
 
